43428e33d6fb36e6cc2047fe011f29c22dfc20ca
AsharamA10 <108696444+AsharamA10@users.noreply.github.com>
2023-12-17 22:08:20 +0530

    script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function fetchVehicles() {
     return Promise.resolve([
       {
         "id": "6049dbd2-45bc-4e34-9ea2-c82ced0279f1",
-        "alert_type": "Unsafe driving",
+        "alert_type": "Unsafe driver",
         "vehicle_id": "cc70a7e5-8397-4914-bbbb-4d6bb521ec67",
         "driver_friendly_name": "Ramesh",
         "vehicle_friendly_name": "KA12A3456",
